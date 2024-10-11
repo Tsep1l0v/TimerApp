66d9a498ae2dde8b95ceb590ddd76de5e027ade9
Tsep1l0v <w1nd0wstsepilov@yandex.ru>
2024-10-11 13:53:22 +0300

    Initial Commit

---
+++ TimerApp/TimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAppApp.swift
+//  TimerApp
+//
+//  Created by Дмитрий Цепилов on 11.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct TimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
